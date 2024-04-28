a1bf9734d2336063b54256ef38ef2d0d78029966
sciion <sciion@sciion.se>
2024-04-28 17:50:38 +0200

    Remove broken verb

--- src/worddata2.js
+++ src/worddata2.js
@@ -36,7 +36,6 @@ export const wordData = {
     {"kanji":"出<rt>で<\/rt>かける","type":"ru","eng":"gå ut"},
     {"kanji":"教<rt>おし<\/rt>える","type":"ru","eng":"undervisa, berätta"},
     {"kanji":"電<rt>でわ<\/rt>話する","type":"irv","eng":"ringa"},
-    {"kanji":"掃<rt>そうじ<\/rt>除する","type":"irv","eng":"städa"},
     {"kanji":"持<rt>も<\/rt>ってくる","type":"irv","eng":"ta med (objekt)"},
     {"kanji":"連<rt>つ<\/rt>れる","type":"ru","eng":"ta med (person)"},
     {"kanji":"おしえる","type":"ru","eng":"att undervisa"},
